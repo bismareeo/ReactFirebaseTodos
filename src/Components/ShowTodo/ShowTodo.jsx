54beadda87ae76fd7766f3492442c6f244d22e39
Bismar Eyner <bismareeo@gmail.com>
2019-05-27 10:27:34 -0400

    showTodos implemented
    Add ShowTodo component.

---
+++ src/Components/ShowTodo/ShowTodo.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ShowTodo.css';
+
+export default class ShowTodo extends React.Component {
+  render() {
+    const todos = this.props.todos;
+    const showTodos = todos.map(todo => {
+      return (
+        <div key={todo.key} className="todo-content">
+          <p> <strong> Title: </strong> {todo.title}</p>
+          <p> <strong> Description: </strong> {todo.description}</p>
+        </div>
+      )
+    });
+    return (
+      <div className="container">
+        <h3 className="title-container">My Todo' List</h3>
+        {showTodos}
+      </div>
+    );
+  }
+}
--- src/Containers/Todos/Todos.jsx
+++ src/Containers/Todos/Todos.jsx
@@ -3,6 +3,7 @@ import Firebase from 'firebase';
 import { config } from '../../config';
 
 import SaveTodo from '../../Components/SaveTodo/SaveTodo';
+import ShowTodo from '../../Components/ShowTodo/ShowTodo';
 
 export default class Todos extends React.Component {
   constructor(props) {
@@ -13,30 +14,43 @@ export default class Todos extends React.Component {
     };
   }
 
-  writeTodos = () => {
-    Firebase.database().ref('/').set(this.state);
-    // console.log('DATA SAVED');
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState !== this.state) {
-      this.writeTodos();
-    }
-  }
-  
-
   onSaveTodo = (title, description) => {
-
     Firebase.database().ref('todos').push({
       title,
       description,
     });
-      
+
+  }
+
+  objectToArray = obj => {
+    const arr = [];
+    for(let key in obj) {
+      const newObj = {
+        key,
+        title: obj[key].title,
+        description: obj[key].description,
+      };
+      arr.push(newObj);
+    }
+    return arr;
+  }
+
+  componentDidMount() {
+    Firebase.database().ref('/').on('value', data => {
+      const todos = data.val().todos;
+      const arrTodos = this.objectToArray(todos);
+      this.setState({
+        todos: arrTodos,
+      });
+    })
   }
 
   render() {
     return (
-      <SaveTodo onSaveTodo={this.onSaveTodo}/>
+      <div>
+        <SaveTodo onSaveTodo={this.onSaveTodo}/>
+        <ShowTodo todos={this.state.todos}/>
+      </div>
     );
   }
 }
